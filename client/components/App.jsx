8f3838ceeac042896766101851e3ecec9dc73899
Jonathan Kwak <jkwak09@gmail.com>
2020-08-25 17:23:31 -0400

    Write basic class component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,2 +1,19 @@
 import React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props) {
+
+    };
+    //function binds here
+
+  };
+
+  render() {
+    return(
+
+    )
+  };
+};
+
+export default App;
