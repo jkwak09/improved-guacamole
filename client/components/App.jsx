4de7691afb62115b85264c0bcc29b141d6197ed2
Jonathan Kwak <jkwak09@gmail.com>
2020-08-25 17:58:16 -0400

    Start building form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {message: ''};
     //function binds here
-
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   };
+
+  handleChange(event) {
+    this.setState({message: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A message was submitted: ' + this.state.message);
+    event.preventDefault();
+  }
 
   render() {
     return(
       <>
-      <div>Hello World</div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Message:
+            <input type="text" name="message" onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </>
     )
   };
