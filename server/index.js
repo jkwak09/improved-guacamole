3a531de41e0439fcad46a432e1feea52302eb9cd
Jonathan Kwak <jkwak09@gmail.com>
2020-08-25 17:37:09 -0400

    Start writing post method

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const port = 3000;
 
 app.use(express.static(__dirname + '/../public'));
 
+//Create
+app.post('/post', (req, res) =>{
+  console.log(req.body);
+
+});
+
 app.listen(port, function() {
   console.log('Listening on port ' + port);
 });
